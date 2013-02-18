android/platform_external_webkit

Avoid members with the same base name in ar's archives
Change-Id: Iecdd8e5b09db21a07d9f0455e19deb4aa7fbcd03
Author: Dmitry Shkurko <Dmitry.V.Shkurko@intel.com>
Signed-off-by: Dmitry Shkurko <Dmitry.V.Shkurko@intel.com>
Singed-off-by: Shuo Gao <shuo.gao@intel.com>
Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
Signed-off-by: Jack Ren <jack.ren@intel.com>
Author-tracking-BZ:47091

Source/WebKit/Android.mk
@@ -63,7 +63,7 @@ LOCAL_SRC_FILES += \
 	android/jni/GeolocationPermissionsBridge.cpp \
 	android/jni/JavaBridge.cpp \
 	android/jni/JavaSharedClient.cpp \
-	android/jni/MIMETypeRegistry.cpp \
+	android/jni/MIMETypeRegistry_jni.cpp \
 	android/jni/MockGeolocation.cpp \
 	android/jni/PicturePile.cpp \
 	android/jni/WebCoreFrameBridge.cpp \

burstlam/android_external_webkit

Change-Id: I4f45bba09476b9e8fbc5c73761addcec964962d4

Source/WebKit/Android.mk
@@ -67,7 +67,7 @@ LOCAL_SRC_FILES += \
 	android/jni/GeolocationServiceBridge.cpp \
 	android/jni/JavaBridge.cpp \
 	android/jni/JavaSharedClient.cpp \
-	android/jni/MIMETypeRegistry.cpp \
+	android/jni/MIMETypeRegistryAndroid.cpp \
 	android/jni/MockGeolocation.cpp \
 	android/jni/PicturePile.cpp \
 	android/jni/WebCoreFrameBridge.cpp \
@@ -105,7 +105,7 @@ LOCAL_SRC_FILES += \
 	android/plugins/SkANP.cpp \
 	\
 	android/wds/Command.cpp \
-	android/wds/Connection.cpp \
+	android/wds/ConnectionAndroid.cpp \
 	android/wds/DebugServer.cpp
 
 LOCAL_C_INCLUDES += \